68072dfe6e1363215180229d6bcb2c33e7803334
yamadapc <tacla.yamada@gmail.com>
2017-04-10 18:57:20 -0300

    Improve the behavior

--- handler.js
+++ handler.js
@@ -11,14 +11,61 @@ function parseMime(str) {
 }
 
 exports.convert = function(event, context, cb) {
+  console.log(event);
   const query = event.queryStringParameters || {};
   const mimeInput = query.inputFormat || parseMime(event.headers['Content-Type']) || 'json';
   const mimeOutput = query.outputFormat || parseMime(event.headers['Accept']) || 'yaml';
   const inp = event.body;
+
+  if (!inp) {
+    cb(null, {
+      statusCode: 400,
+      body: '400 Send some text\n',
+    });
+    return;
+  }
+
   const inputFormat = configuration[mimeInput];
+  if (!inputFormat) {
+    cb(null, {
+      statusCode: 400,
+      body: `400 Invalid 'inputFormat' ${mimeInput}, send it with 'content-type' or '?inputFormat'\n`,
+    });
+    return;
+  }
   const outputFormat = configuration[mimeOutput];
+  if (!outputFormat) {
+    cb(null, {
+      statusCode: 400,
+      body: `400 Invalid 'outputFormat' ${mimeOutput}, send it with 'accept' or '?outputFormat'\n`,
+    });
+    return;
+  }
+
+  let tmp;
+  try {
+    tmp = inputFormat.parse(inp);
+  } catch (err) {
+    cb(null, {
+      statusCode: 422,
+      body: `422 Failed to parse body as '${mimeInput}':\nError: ${err.message}\n`,
+    });
+    return;
+  }
+
+  let body;
+  try {
+    body = outputFormat.stringify(tmp);
+  } catch (err) {
+    cb(null, {
+      statusCode: 422,
+      body: `422 Failed to generate output '${mimeOutput}':\nError: ${err.message}\n`,
+    });
+    return;
+  }
+
   cb(null, {
     statusCode: 200,
-    body: outputFormat.stringify(inputFormat.parse(inp))
+    body
   });
 };
